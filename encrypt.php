a49249e935ea553f06de2474a49abfa54655a4f7
wanmin <min.wan@yoho.cn>
2016-12-31 17:33:02 +0800

    产生密码与比较

--- encrypt.php
+++ encrypt.php
@@ -100,10 +100,290 @@ class Encrypt
         return self::auth($string, $key, $expiry, __FUNCTION__);
     }
 
-}
 
+    /**
+     * 获取编码
+     *
+     * @param String $plaintext
+     * @param String $salt
+     * @param String $encryption
+     * @param bool $show_encrypt
+     * @return String
+     */
+    public static function getCrypted($plaintext, $salt = '', $encryption = 'md5-hex', $show_encrypt = false)
+    {
+        $salt = self::getSalt($encryption, $salt, $plaintext);
+        switch ($encryption)
+        {
+            case 'plain' :
+                return $plaintext;
+            case 'sha' :
+                $encrypted = base64_encode(mhash(MHASH_SHA1, $plaintext));
+                return ($show_encrypt) ? '{SHA}' . $encrypted : $encrypted;
+            case 'crypt' :
+            case 'crypt-des' :
+            case 'crypt-md5' :
+            case 'crypt-blowfish' :
+                return ($show_encrypt ? '{crypt}' : '') . crypt($plaintext, $salt);
+            case 'md5-base64' :
+                $encrypted = base64_encode(mhash(MHASH_MD5, $plaintext));
+                return ($show_encrypt) ? '{MD5}' . $encrypted : $encrypted;
+            case 'ssha' :
+                $encrypted = base64_encode(mhash(MHASH_SHA1, $plaintext . $salt) . $salt);
+                return ($show_encrypt) ? '{SSHA}' . $encrypted : $encrypted;
+            case 'smd5' :
+                $encrypted = base64_encode(mhash(MHASH_MD5, $plaintext . $salt) . $salt);
+                return ($show_encrypt) ? '{SMD5}' . $encrypted : $encrypted;
+            case 'aprmd5' :
+                $length = strlen($plaintext);
+                $context = $plaintext . '$apr1$' . $salt;
+                $binary = self::_bin(md5($plaintext . $salt . $plaintext));
+                for ($i = $length; $i > 0; $i -= 16)
+                {
+                    $context .= substr($binary, 0, ($i > 16 ? 16 : $i));
+                }
+                for ($i = $length; $i > 0; $i >>= 1)
+                {
+                    $context .= ($i & 1) ? chr(0) : $plaintext[0];
+                }
+                $binary = self::_bin(md5($context));
+
+                for ($i = 0; $i < 1000; $i++)
+                {
+                    $new = ($i & 1) ? $plaintext : substr($binary, 0, 16);
+                    if ($i % 3)
+                    {
+                        $new .= $salt;
+                    }
+                    if ($i % 7)
+                    {
+                        $new .= $plaintext;
+                    }
+                    $new .= ($i & 1) ? substr($binary, 0, 16) : $plaintext;
+                    $binary = self::_bin(md5($new));
+                }
+
+                $p = array();
+                for ($i = 0; $i < 5; $i++)
+                {
+                    $k = $i + 6;
+                    $j = $i + 12;
+                    if ($j == 16)
+                    {
+                        $j = 5;
+                    }
+                    $p[] = self::_toAPRMD5((ord($binary[$i]) << 16) | (ord($binary[$k]) << 8) | (ord($binary[$j])), 5);
+                }
+
+                return '$apr1$' . $salt . '$' . implode('', $p) . self::_toAPRMD5(ord($binary[11]), 3);
+
+            case 'md5-hex' :
+
+            default :
+                $encrypted = ($salt) ? md5($plaintext . $salt) : md5($plaintext);
+                return ($show_encrypt) ? '{MD5}' . $encrypted : $encrypted;
+        }
+    }
+
+    /**
+     * Enter description here...
+     *
+     * @param unknown_type $encryption
+     * @param unknown_type $seed
+     * @param unknown_type $plaintext
+     * @return unknown
+     */
+    private static function getSalt($encryption = 'md5-hex', $seed = '', $plaintext = '')
+    {
+        switch ($encryption)
+        {
+            case 'crypt' :
+            case 'crypt-des' :
+                if ($seed)
+                {
+                    return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 2);
+                }
+                else
+                {
+                    return substr(md5(mt_rand()), 0, 2);
+                }
+                break;
+
+            case 'crypt-md5' :
+                if ($seed)
+                {
+                    return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 12);
+                }
+                else
+                {
+                    return '$1$' . substr(md5(mt_rand()), 0, 8) . '$';
+                }
+                break;
+
+            case 'crypt-blowfish' :
+                if ($seed)
+                {
+                    return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 16);
+                }
+                else
+                {
+                    return '$2$' . substr(md5(mt_rand()), 0, 12) . '$';
+                }
+                break;
+
+            case 'ssha' :
+                if ($seed)
+                {
+                    return substr(preg_replace('|^{SSHA}|', '', $seed), -20);
+                }
+                else
+                {
+                    return mhash_keygen_s2k(MHASH_SHA1, $plaintext, substr(pack('h*', md5(mt_rand())), 0, 8), 4);
+                }
+                break;
+
+            case 'smd5' :
+                if ($seed)
+                {
+                    return substr(preg_replace('|^{SMD5}|', '', $seed), -16);
+                }
+                else
+                {
+                    return mhash_keygen_s2k(MHASH_MD5, $plaintext, substr(pack('h*', md5(mt_rand())), 0, 8), 4);
+                }
+                break;
+
+            case 'aprmd5' :
+                /* 64 characters that are valid for APRMD5 passwords. */
+                $APRMD5 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+                if ($seed)
+                {
+                    return substr(preg_replace('/^\$apr1\$(.{8}).*/', '\\1', $seed), 0, 8);
+                }
+                else
+                {
+                    $salt = '';
+                    for ($i = 0; $i < 8; $i++)
+                    {
+                        $salt .= $APRMD5{rand(0, 63)};
+                    }
+                    return $salt;
+                }
+                break;
+
+            default :
+                $salt = '';
+                if ($seed)
+                {
+                    $salt = $seed;
+                }
+                return $salt;
+                break;
+        }
+    }
+
+    private static function genRandom($length = 8)
+    {
+        $salt = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $len = strlen($salt);
+        $makepass = '';
+
+        $stat = @stat(__FILE__);
+        if (empty($stat) || !is_array($stat))
+            $stat = array(
+                php_uname()
+            );
+
+        mt_srand(crc32(microtime() . implode('|', $stat)));
+
+        for ($i = 0; $i < $length; $i++)
+        {
+            $makepass .= $salt[mt_rand(0, $len - 1)];
+        }
+
+        return $makepass;
+    }
+
+    private static function _toAPRMD5($value, $count)
+    {
+        /* 64 characters that are valid for APRMD5 passwords. */
+        $APRMD5 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        $aprmd5 = '';
+        $count = abs($count);
+        while (--$count)
+        {
+            $aprmd5 .= $APRMD5[$value & 0x3f];
+            $value >>= 6;
+        }
+        return $aprmd5;
+    }
+
+    private static function _bin($hex)
+    {
+        $bin = '';
+        $length = strlen($hex);
+        for ($i = 0; $i < $length; $i += 2)
+        {
+            $tmp = sscanf(substr($hex, $i, 2), '%x');
+            $bin .= chr(array_shift($tmp));
+        }
+        return $bin;
+    }
+
+    /**
+     * 组合一个安全的密码
+     * @param $code
+     * @return String
+     */
+    public static function makePass($code)
+    {
+        $salt = self::genRandom(32);
+        $crypt = self::getCrypted($code . $salt, $salt);
+        return $crypt . ':' . $salt;
+    }
+
+    /**
+     * 验证密码
+     *
+     * @param String $inputPassword
+     * @param String $password
+     * @return bool
+     */
+    public static function authPassword($inputPassword, $password)
+    {
+        if (empty($password) || empty($inputPassword))
+        {
+            return false;
+        }
+        $passwordList = explode(':', trim($password));
+        if (count($passwordList) != 2)
+        {
+            return false;
+        }
+        list($crypt, $salt) = $passwordList;
+        $decode = self::getCrypted($inputPassword . $salt, $salt);
+        if ($crypt != $decode)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+}
+//简单使用
 $key = 'wsd222222222222111222ddee';
 $data = new Encrypt();
+//加密
 $enString = $data->encode("abcdefghijk!!",$key);
 echo $enString."\n";
+//解密
 echo $data->decode($enString,$key);
+echo "\n\n***************\n\n";
+
+//或者生产使用
+$passwd = $data->makePass("adads!#%%$");//产生密码
+echo $passwd."\n";
+var_dump($data->authPassword("adads!#%%$",$passwd));//比较
+
